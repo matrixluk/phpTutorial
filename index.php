0b3e01d64965ddfbcaa712327743bc64a9846f04
Łukasz Karbowniczek <matrixluk@gmail.com>
2019-05-12 00:59:16 -0700

    Last version POST, with better structure

--- index.php
+++ index.php
@@ -6,18 +6,6 @@ $header = "Kalkulator";
 
 $sum = 0;
 
-$x = isset($_POST['x'])? (float)$_POST['x'] : 0.0;
-$y = isset($_POST['y'])? (float)$_POST['y'] : 0.0;
-
-//sprawdzamy czy x i y zostaly przeslane
-//oraz sprawdzamy czy sa liczbami bo tak to idzie tekst
-if (($x || $x === 0.0) &&
-    ($y || $y===0.0 ) &&
-    is_numeric($x) &&
-    is_numeric($y)) {
-
-            $sum = $x + $y;
-   }
 
 
 
---
+++ result.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = isset($_POST['x'])? (float)$_POST['x'] : 0.0;
+$y = isset($_POST['y'])? (float)$_POST['y'] : 0.0;
+
+//sprawdzamy czy x i y zostaly przeslane
+//oraz sprawdzamy czy sa liczbami bo tak to idzie tekst
+if (($x || $x === 0.0) &&
+    ($y || $y===0.0 ) &&
+    is_numeric($x) &&
+    is_numeric($y)) {
+
+    $sum = $x + $y;
+}
+
+echo $sum;
